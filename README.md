b7adf5f0b73cac38ea8a3937682779da5211be51
LAPTOP-7V78BBO2\ydf19 <yangdf@akulaku.com>
2023-03-15 11:11:38 +0800

    no message

--- README.md
+++ README.md
@@ -6,3 +6,20 @@ pip install nb_libs
 
 # 1. restart_program每隔多久重启代码自身。
 
+```python
+import time
+import datetime
+from nb_libs.restart_programe_by_interval import restart_program
+
+
+def _run():
+    print(datetime.datetime.now(), '开始运行程序')
+    for i in range(1000):
+        time.sleep(0.5)
+        print(datetime.datetime.now(), i)
+
+
+if __name__ == '__main__':
+    restart_program(10)  # 每隔10秒重启。
+    _run()
+```
--- tests/test_restart_programe_by_interval.py
+++ tests/test_restart_programe_by_interval.py
@@ -1,4 +1,4 @@
-﻿
+﻿import os
 import time
 import nb_log
 from nb_libs.restart_programe_by_interval import restart_program
@@ -8,11 +8,11 @@ from multiprocessing import Process
 print('start')
 
 def f():
-    while 1:
-        time.sleep(1)
-        print('hi')
+    for i in range(1000):
+        time.sleep(5)
+        print(os.getpid(),'hi',i)
 
 if __name__ == '__main__':
-    restart_program(time_sleep=12)
+    restart_program(10)
     Process(target=f).start()
     Process(target=f).start()
