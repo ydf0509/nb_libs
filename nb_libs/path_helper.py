0a081f12896b14d807bfee99fc3f06a9aa742e99
LAPTOP-7V78BBO2\ydf19 <yangdf@akulaku.com>
2024-04-01 15:55:51 +0800

    commit

--- nb_libs/path_helper.py
+++ nb_libs/path_helper.py
@@ -1,9 +1,13 @@
+import importlib.util
 import os
+import sys
+import types
 import typing
 from pathlib import Path, WindowsPath, PosixPath
+from nb_log import LoggerMixin
 
 
-class PathHelper:
+class PathHelper(LoggerMixin):
     def __init__(self, path: typing.Union[os.PathLike, str]):
         self.path = Path(path)
 
@@ -25,7 +29,7 @@ class PathHelper:
                 # print('dir: ', entry)
         return files
 
-    def rglob_dirs(self, pattern: str,  is_resolve: bool = False) -> typing.List[Path]:
+    def rglob_dirs(self, pattern: str, is_resolve: bool = False) -> typing.List[Path]:
         entries = self.path.rglob(pattern, )
         dirs = []
         # 遍历文件夹和文件
@@ -43,9 +47,31 @@ class PathHelper:
                 dirs.append(entry)
         return dirs
 
+    def import_as_module(self, module_name: str = None) -> types.ModuleType:
+        if module_name is None:
+            module_name = self.path.parent.as_posix().replace('/', '.') + '.' + self.path.stem
+
+        module_spec = importlib.util.spec_from_file_location(module_name, self.path)
+        module = importlib.util.module_from_spec(module_spec)
+        module_spec.loader.exec_module(module)
+        return module
+        # except FileNotFoundError:
+        #     self.logger.exception(f"Module file '{self.path}' not found.")
+        # except Exception as e:
+        #     self.logger.exception(f"Failed to import module '{module_name}': {str(e)}")
+
+    def auto_import_files_in_dir(self, ) -> None:
+        for file_path in self.rglob_files('*.py'):
+            self.logger.debug(f'导入模块 {file_path}')
+            if Path(file_path) == Path(sys._getframe(1).f_code.co_filename):
+                self.logger.warning(f'排除导入调用PathHelper的模块自身 {file_path}')  # 否则下面的import这个文件,会造成无限懵逼死循环
+                continue
+            self.__class__(file_path).import_as_module()
+
 
 if __name__ == '__main__':
     print(type(Path('/')))
-    print(list(PathHelper(r'../').rglob_files('*',is_resolve=True)))
+    print(list(PathHelper(r'../').rglob_files('*', is_resolve=True)))
+    print(PathHelper(r'D:\codes\nb_libs\nb_libs/dict2json.py').import_as_module().__dict__)
 
-    print(Path(r'D:\codes\nb_libs\nb_libs\..\tests\tests_time.py').resolve())
+    PathHelper('../').auto_import_files_in_dir()
