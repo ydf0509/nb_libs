21e1125c1f9cde5db2264d78f897310f6b74f65f
LAPTOP-7V78BBO2\ydf19 <yangdf@akulaku.com>
2024-05-28 11:53:56 +0800

    auto commit

--- nb_libs/github_git_clone.py
+++ nb_libs/github_git_clone.py
@@ -10,6 +10,8 @@ from parsel import Selector
 """
 自动下载和更新某人所有的github代码,仓库太多了,手动下载很麻烦.
 """
+
+
 class GithubCloner(nb_log.LoggerMixin):
     def __init__(self, username: str, total_pages: int = 3, dir='/codes_github'):
         self.username = username
@@ -17,6 +19,7 @@ class GithubCloner(nb_log.LoggerMixin):
         self.repo_list = []
         self.dir = dir
         os.makedirs(self.dir, exist_ok=True)
+        os.system('chcp 65001')  # 先设置utf8编码,避免os.system 乱码
 
     def get_repo_urls(self):
         for p in range(1, self.total_pages + 1):
@@ -27,7 +30,7 @@ class GithubCloner(nb_log.LoggerMixin):
             time.sleep(10)
         self.logger.info(len(self.repo_list))
 
-    def clone_repo(self, repo: str):
+    def clone_one_repo(self, repo: str):
         url = f'git@github.com:{repo[1:]}.git'
 
         repo_short_name = repo.split('/')[-1]
@@ -45,14 +48,28 @@ class GithubCloner(nb_log.LoggerMixin):
             os.system(cmd)
             time.sleep(30)
 
-    def start(self):
+    def start_clone(self):
         self.get_repo_urls()
-        os.system('chcp 65001')  # 先设置utf8编码,避免os.system 乱码
+
         for repo in self.repo_list:
-            self.clone_repo(repo)
+            self.clone_one_repo(repo)
+
+    def push_one_repo(self,folder):
+        folder = str(folder)
+        cmd_list =[f'cd {folder}','git pull','git diff','git add ./.','git commit -m "auto commit"','git push origin','git push github']
+        cmd = ' && '.join(cmd_list)
+        self.logger.info(cmd)
+        os.system(cmd)
+
+    def start_push(self):
+        for folder in Path(self.dir).iterdir():
+            self.push_one_repo(folder)
+
+
 
 
 if __name__ == '__main__':
-    GithubCloner('ydf0509', total_pages=2, dir='/codes_github').start()
+    # GithubCloner('ydf0509', total_pages=2, dir='/codes_github').start_clone()
+    GithubCloner('ydf0509', total_pages=2, dir='/codes').start_push()
 
     # os.system(''' cd /codes_github/nb_log2 && git pull  ''')
